449a54c9414fbbfd52affae08c33344d289d4e50
Heitor C <hcproradio@gmail.com>
2014-06-12 12:47:46 -0300

    send login to admin

--- tinacg/tinacg/urls.py
+++ tinacg/tinacg/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from django.conf import settings
 
 from django.contrib import admin
@@ -14,6 +15,8 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^vertfolia/', include('vertfolia.urls')),
                            
+                       url(r'^accounts/login', RedirectView.as_view(
+                           url="/admin/", permanent=False)),
 )
 
 urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
